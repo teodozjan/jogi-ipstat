dad3e9c3c04326ca93058954a38b0bb239c5bb39
Kamil Kułaga <teodozjan@gmail.com>
2014-02-21 19:52:39 +0100

    New attitude for regexes

--- lib/Jogi/Regex.pm
+++ lib/Jogi/Regex.pm
@@ -2,20 +2,11 @@
 
 use v6;
 
-module Jogi::Regex{
-
-
-
-
-  sub get_regex is export(){
-    my $func = shift @_ || "syslog";
-    return('.{15} \w+ (\w+)') if $func eq "syslog";
-    return('.+? (\S+): ') if $func eq "vsftp";
-    return('.{36} (\S+)') if $func eq "exim";
-    return('.{15} (\S+)') if $func eq "tcpdump";
-    return('(.+)') if $func eq "all";
-
-
-  }
+grammar Jogi::Regex{
+    rule TOP {<stringed><ip><stringed>}
+    rule stringed {.+}
+    token byte {(\d**1..3) <?{ $0 < 256 }>}
+    token ip {<byte> [\. <byte>] ** 3}
 }
+
 
