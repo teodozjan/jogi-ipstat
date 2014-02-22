ca1970a92e3aed9c1884ab1df0df6c20c7d3fc5a
Kamil Kułaga <teodozjan@gmail.com>
2014-02-22 17:24:05 +0100

    almost works

--- lib/Jogi/Regex.pm
+++ lib/Jogi/Regex.pm
@@ -3,15 +3,15 @@
 use v6;
 
 grammar Jogi::Ip{
-    token TOP{^<logrecord>+$}
+    regex TOP{^<logrecord>+$}
 
-    #token byte {(\d**1..3) <?{ $0 < 256 }>}
-    #token ip {<byte> [\. <byte>] ** 3}
-    token stamp{<alpha>**3 \s \d**2 \s \d**2 ':' \d**2 ':' \d**2 }
-    token hostname{\w+}
-    token service{\w+}
-    token message{\w+}
-    token logrecord {<stamp>\s<hostname>\s+<service>':'\s <message>}
+    #regex byte {(\d**1..3) <?{ $0 < 256 }>}
+    #regex ip {<byte> [\. <byte>] ** 3}
+    regex stamp{<alpha>**3 \s \d**2 \s \d**2 ':' \d**2 ':' \d**2 }
+    regex hostname{\w+}
+    regex service{\w+}
+    regex message{\w+}
+    regex logrecord {<stamp>\s<hostname>\s+<service>':'\s <message>}
 
 
 }
