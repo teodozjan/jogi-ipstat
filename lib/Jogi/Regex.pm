e6b9ebc2acf972b371fefd766544fb8269413159
Kamil Kułaga <teodozjan@gmail.com>
2014-02-22 12:46:03 +0100

    Making jogi raw again

--- lib/Jogi/Regex.pm
+++ lib/Jogi/Regex.pm
@@ -2,11 +2,12 @@
 
 use v6;
 
-grammar Jogi::Regex{
-    rule TOP {<stringed><ip><stringed>}
-    rule stringed {.+}
-    token byte {(\d**1..3) <?{ $0 < 256 }>}
-    token ip {<byte> [\. <byte>] ** 3}
-}
+grammar Jogi::Regex;
+
+#rule TOP {<stringed><ip><stringed>}
+rule stringed {.+}
+rule byte {(\d**1..3) <?{ $0 < 256 }>}
+rule ip {<byte> [\. <byte>] ** 3}
+
 
 
