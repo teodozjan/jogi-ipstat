ac16a5be43137df6b4cf601eaccc128790115e55
Kamil Kułaga <teodozjan@gmail.com>
2014-02-22 17:16:52 +0100

    almost works

--- lib/Jogi/Regex.pm
+++ lib/Jogi/Regex.pm
@@ -2,12 +2,52 @@
 
 use v6;
 
-grammar Jogi::Regex;
+grammar Jogi::Ip{
+    token TOP{^<logrecord>+$}
 
-#rule TOP {<stringed><ip><stringed>}
-rule stringed {.+}
-rule byte {(\d**1..3) <?{ $0 < 256 }>}
-rule ip {<byte> [\. <byte>] ** 3}
+    #token byte {(\d**1..3) <?{ $0 < 256 }>}
+    #token ip {<byte> [\. <byte>] ** 3}
+    token stamp{<alpha>**3 \s \d**2 \s \d**2 ':' \d**2 ':' \d**2 }
+    token hostname{\w+}
+    token service{\w+}
+    token message{\w+}
+    token logrecord {<stamp>\s<hostname>\s+<service>':'\s <message>}
+
+
+}
+
+class Jogi::SysLogRecord{
+    my $stamp;
+    my $service;
+    my $ip;
+
+}
+
+class Jogi::IpActions{
+    method TOP($/){
+	say "TOP $/";
+    }
+
+    method logrecord($/){
+	say "logrecord $/";
+    }   
+
+    method stamp($/){
+	say "stamp $/";
+    } 
+
+    method message($/){
+	say "message $/";
+    }
+
+    method hostname($/){
+	say "hostname $/";
+    }
+
+    method service($/){
+	say "service $/";
+    }
+}
 
 
 
