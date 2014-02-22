d050672912d4be35eda765774f614500883a2b8f
Kamil Kułaga <teodozjan@gmail.com>
2014-02-22 23:13:54 +0100

    Now service[pid] is being caught

--- lib/Jogi/Regex.pm
+++ lib/Jogi/Regex.pm
@@ -3,15 +3,15 @@
 use v6;
 
 grammar Jogi::Ip{
-    token TOP{^<logrecord>+$}
+    token TOP{^<logrecord>+?$}
 
     #token byte {(\d**1..3) <?{ $0 < 256 }>}
     #token ip {<byte> [\. <byte>] ** 3}
     token stamp{<alpha>**3 \s \d**2 \s \d**2 ':' \d**2 ':' \d**2 }
     token hostname{\w+}
-    token service{\w+}
+    token service{\N+?':'}
     token message{.+?\n}
-    token logrecord {<stamp>\s+<hostname>\s+<service>':'\s+<message>}
+    token logrecord {<stamp>\s+<hostname>\s+<service>\s+<message>}
 
 
 }
