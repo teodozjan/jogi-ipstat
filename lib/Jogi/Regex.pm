4c62469e6ed5421e75ee74477a956fd131e118ea
Kamil Kułaga <teodozjan@gmail.com>
2014-02-24 11:04:45 +0100

    Last hacked version

--- lib/Jogi/Regex.pm
+++ lib/Jogi/Regex.pm
@@ -3,14 +3,14 @@
 use v6;
 
 grammar Jogi::Ip{
-    token TOP{<logrecord>+}
+    token TOP{^<logrecord>+$}
     #rule byte {(<digit>**1..3) <?{ $0 < 256 }>}
     #rule ip {<byte> [\. <byte>] ** 3}
     token stamp{<alpha>**3 <space> <digit>**2 <space> <digit>**2 ':' <digit>**2 ':' <digit>**2 }
     token hostname{<-space>+}
     token service{<-space>+}
     token message{.+?\n+}#<-space>+}
-    token logrecord {^^<stamp><space><hostname><space><service>:<message>}
+    token logrecord {<stamp><space><hostname><space><service>:<message>}
  
    
 }
@@ -24,7 +24,7 @@ class Jogi::SysLogRecord{
 
 class Jogi::IpActions{
     method TOP($/){
-	say "PTOP $/";
+	say "Parsing end";
     }
 
     method logrecord($/){
